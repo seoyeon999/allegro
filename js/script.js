504def14b450e061114ea0a51f50d4649b046f9d
seoyeon999 <162558606+seoyeon999@users.noreply.github.com>
2024-06-03 21:06:02 +0900

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -3,7 +3,7 @@ $(document).ready(function () {
     var jbOffset = $('header').offset();
     /*선택자는 본인 구조에 맞게 수정*/
     $(window).scroll(function () {
-        if ($(document).scrollTop() > 969) {//여기 지정된 길이 이후부터 적용됩니다
+        if ($(document).scrollTop() > 700) {//여기 지정된 길이 이후부터 적용됩니다
             $('header').addClass("header_scroll");
             /*선택자는 본인 구조에 맞게 수정, on클래스 만들어서 변경 원하는 스타일 주세요*/
         }
@@ -37,19 +37,6 @@ $(function () {
     });
 });
 
-
-//cursor
-document.addEventListener("mousemove", e => {
-    gsap.to(".text", {
-        x: e.clientX,
-        y: e.clientY,
-        stagger: { // wrap advanced options in an object
-            each: -0.02,
-            ease: "power2.inOut",
-        }
-    })
-})
-
 //move to <a>
 $(function () {
     $('a[href*="#"]:not([href="#"])').click(function () {
@@ -63,5 +50,25 @@ $(function () {
                 return false;
             }
         }
+    });
+});
+
+//cursor
+document.addEventListener("mousemove", e => {
+    gsap.to(".text", {
+        x: e.clientX,
+        y: e.clientY,
+        stagger: { // wrap advanced options in an object
+            each: -0.02,
+            ease: "power2.inOut",
+        }
+    })
+})
+
+//purchase
+$(function () {
+    $(".purchase").mouseup(function () {
+        alert("로그인이 필요한 서비스입니다.");
+        location.href = 'login.html';
     });
 });
